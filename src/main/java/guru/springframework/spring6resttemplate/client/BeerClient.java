4c50e2a2fca090796624863abce6a507fc8e8294
goose <goose@si>
2025-06-04 17:26:36 -0600

    Finished up to Assignment 22 in section 20.

---
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6resttemplate.client;
+
+import org.springframework.data.domain.Page;
+
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
+
+public interface BeerClient {
+	Page<BeerDTO> listBeers();
+	Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber, Integer pageSize);
+}
---
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6resttemplate.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeerClientImplTest {
+	@Autowired
+	BeerClientImpl beerClient;
+	
+	@Test
+	void listBeersNoBeerName() {
+		beerClient.listBeers();
+	}
+	
+	@Test
+	void listBeers() {
+		beerClient.listBeers("ALE", null, null, null, null);
+	}
+}
